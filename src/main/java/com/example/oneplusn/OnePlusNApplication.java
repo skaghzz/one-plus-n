690d2fd28f7bf7e93fc3efcad23d6450bab96802
namhokim <skaghzz@naver.com>
2021-07-08 15:24:52 +0900

    first commit

---
+++ src/main/java/com/example/oneplusn/OnePlusNApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneplusn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePlusNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePlusNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oneplusn/OnePlusNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneplusn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePlusNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
