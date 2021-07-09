5300b7422722e30b5a22ecaf2e03ab2d1dd70235
namhokim <skaghzz@naver.com>
2021-07-09 17:44:31 +0900

    엔티티 변경사항 추가

--- src/main/java/com/example/oneplusn/domain/Customer.java
+++ src/main/java/com/example/oneplusn/domain/Customer.java
@@ -3,6 +3,7 @@ package com.example.oneplusn.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -18,7 +19,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
 public class Customer {
     @Id
     @GeneratedValue
@@ -28,6 +28,12 @@ public class Customer {
     @Column(name = "NAME")
     private String name;
 
-    @OneToMany(mappedBy = "id", fetch = FetchType.LAZY)
+    //@OneToMany(mappedBy = "id", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "id", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<SavingsAccount> savingsAccount = new ArrayList<>();
+
+    @Builder
+    public Customer(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/oneplusn/domain/SavingsAccount.java
+++ src/main/java/com/example/oneplusn/domain/SavingsAccount.java
@@ -1,11 +1,12 @@
 package com.example.oneplusn.domain;
 
-import java.math.BigDecimal;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -24,8 +25,13 @@ public class SavingsAccount {
     @Column(name = "NAME")
     private String name;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
     @Builder
-    public SavingsAccount(String name) {
+    public SavingsAccount(Customer customer, String name) {
         this.name = name;
+        this.customer = customer;
     }
 }
