ec398b02a3e8ec462332018362bc9ef9f26d9967
namhokim <skaghzz@naver.com>
2021-07-09 10:01:58 +0900

    repository 추가(예금계좌, 고객)

---
+++ src/main/java/com/example/oneplusn/repository/SavginsAccountRepository.java
@@ -0,0 +1,9 @@
+package com.example.oneplusn.repository;
+
+import com.example.oneplusn.domain.SavingsAccount;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SavginsAccountRepository extends JpaRepository<SavingsAccount, Long> {
+
+}
